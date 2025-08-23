ee93d6bdf3c6f28e0dbd08ba567c479608c03d3e
Tran Van Cong <aanh7108@gmail.com>
2025-08-23 22:06:31 +0700

    fix lỗi hiện thị voucher

--- src/components/VoucherSelectorComponent/VoucherSelectorComponent.jsx
+++ src/components/VoucherSelectorComponent/VoucherSelectorComponent.jsx
@@ -27,9 +27,10 @@ const VoucherSelectorComponent = ({ cartTotal, onSelect }) => {
         <List
           dataSource={voucherData?.data}
           renderItem={(item) => {
-            const isEligible = cartTotal >= item.minOrderValue&& new Date() >=new Date(item.startDate)&&
-                                (!item.userLimit || 
-                                 !item.usedBy?.some(us => us.userId === user.id && us.count >= item.userLimit));
+             const isEligible = cartTotal >= item.minOrderValue&& new Date() >=new Date(item.startDate)
+                                && new Date() <= new Date(item.endDate)
+                                && (!item.userLimit || 
+                                 item.usedBy?.some(us => us.userId === user.id && us.count < item.userLimit));
 ;
             return (
               <List.Item
